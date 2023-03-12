eb0555b8f211e00030e42b9ffa72af2b8a5251f8
Mike Weatherford <mikesweatherford@gmail.com>
2023-03-11 19:55:44 -0500

    added more

---
+++ MoviesAndFilmWebApp/Enums/MPAARatingCategory.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAndFilmWebApp.Enums
+{
+    enum MPAARatingCategory
+    {
+        [Display(Name = "G")]
+        G,
+        [Display(Name = "PG")]
+        PG,
+        [Display(Name = "PG-13")]
+        PG13,
+        [Display(Name = "R")]
+        R,
+        [Display(Name = "N/A")]
+        NA
+    }
+}
--- MoviesAndFilmWebApp/Models/Movie.cs
+++ MoviesAndFilmWebApp/Models/Movie.cs
@@ -1,15 +1,28 @@
-﻿namespace MoviesAndFilmWebApp.Models
+﻿using MoviesAndFilmWebApp.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAndFilmWebApp.Models
 {
     public class Movie
     {
+        private MPAARatingCategory ratingCategory1;
+
         public Guid Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Stars { get; set; }
+        [Required]
         public string Director { get; set; }
+        [Required]
         public string Writer { get; set; }
+        [Required]
         public string Genre { get; set; }
+        [Range(1.0, 10.0)]
         public double UserRating { get; set; }
+        [Required]
         public string Plot { get; set; }
+
         public string MPAARating { get; set; }
         public string RunTime { get; set; }
         public string ReleaseYear { get; set; }
